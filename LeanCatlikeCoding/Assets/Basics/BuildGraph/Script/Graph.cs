30583b194c604e25ad8c6dfc66a93e5db2d6d969
kof123w <1617034185@qq.com>
2023-06-19 15:51:23 +0800

    init

--- LeanCatlikeCoding/Assets/Basics/BuildGraph/Script/Graph.cs
+++ LeanCatlikeCoding/Assets/Basics/BuildGraph/Script/Graph.cs
@@ -4,35 +4,62 @@ using UnityEngine;
 
 public class Graph : MonoBehaviour
 {
-    [SerializeField] public Transform _pointPrefabs;
-    [Range(0, 100)] public float _pointNum = 10;
+    [SerializeField] public Transform pointPrefabs;
+    [SerializeField] public int createPointNum = 100;  //创建点的数量
+    [SerializeField] public float pointDist = 0.1f;    //点和点之间x轴的距离 
+    [SerializeField] public float zPointDist = 0.05f;  //点和点之间z轴的距离
+    [SerializeField] public int max = 100;
+    [SerializeField] public float scale = 1.0f;
+    private Transform[] _transforms;
+    private Transform _trans; 
     
-    //初始化图形代码
     void Start()
     {
-        float step = 2.0f / _pointNum;
+        _trans = transform; 
        //统一修改预制体大小
-       _pointPrefabs.localScale = Vector3.one * step ;
-       for (int i = 0;i < _pointNum;i++)
+       pointPrefabs.localScale = new Vector3(scale,scale,scale);
+       _transforms = new Transform[createPointNum];
+       for (int i = 0;i<createPointNum;i++)
        {
-           Transform point = Instantiate(_pointPrefabs);
-           //设置创建的子对象
-           point.SetParent(transform); 
+           Transform newTrans = Instantiate(pointPrefabs);
+           newTrans.gameObject.name = $"point{i}";
+           newTrans.SetParent(_trans); 
+           _transforms[i] = newTrans;
        }
-    }
-    
+    } 
+   
     //让图像动起来
     void Update()
     {
+        Tick();
+    }
+
+    [SerializeField] public FunctionLibrary.FuncName funcIndex = FunctionLibrary.FuncName.DrawSin;
+    [HideInInspector] private Vector3 _cache = Vector3.zero; 
+    void Tick()
+    {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunc((int)funcIndex);
         float time = Time.time;
-        float step = 2.0f / _pointNum;
-        for (int i = 0; i < _pointNum; i++)
-        { 
-            Vector3 position = Vector3.zero;
-            Transform point = transform.GetChild(i);
-            position.x = (i + 0.5f) * step - 1f;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time)) ;
-            point.position = position;
+        int index = GetStartX();
+        int zIndex = 0;
+        for (int i = 0;i < _transforms.Length;i++)
+        {
+            float x = index * pointDist;
+            float z = zIndex * zPointDist;
+            _cache.x = x;
+            _cache.y = f(x,z,time);
+            _cache.z = z;
+            _transforms[i].localPosition = _cache;
+            index++;
+            if (index >= max){
+                index = GetStartX();
+                zIndex++;
+            }
         }
+    }
+    
+    int GetStartX()
+    {
+        return -_transforms.Length / 2 <= -max ? -max : -_transforms.Length / 2;
     }
 }
--- LeanCatlikeCoding/Assets/Basics/BuildGraph/Shader/PointShader.shader
+++ LeanCatlikeCoding/Assets/Basics/BuildGraph/Shader/PointShader.shader
@@ -1,52 +1,44 @@
-// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
-
-Shader "Custom/PointShader"
+shader "Custom/PointShader"
 {
-    SubShader
-    {
-       Tags { "RenderType" = "Opaque"}
-       LOD 200
-       
-       Pass{
-           CGPROGRAM 
-           //声明一个顶点着色器
-           #pragma vertex vert
-            //声明一个片元着色器
-           #pragma fragment frag
-           
-           //引入一下Unity自带的CG库UnityCG.cginc
-           #include "UnityCG.cginc"
-           //声明一个结构体用于输入顶点着色器
-           struct ad_data{
-              float4 vertPos:POSITION;   //告诉结构体从模型空间里面拿坐标
-           };
-           
-           //声明一个结构体用于顶点着色器输出，片元着色器输入
-           struct v2f{
-              float4 fragPos:SV_POSITION;  //告诉结构体从顶点着色器输出处那顶点坐标
-              float4 fragData:TEXCOORD0;    //把数据输出到TEXCOORD0处进行存放
-           };
-           
-           //编写顶点着色器函数。SV_POSITION为输出位置
-           v2f vert(ad_data i):SV_POSITION{
-                v2f o;    //声明输出结构体
-                //UnityObjectToClipPos为Unity Shader内置函数。从模型空间坐标转换成裁剪空间坐标 
-                o.fragPos = UnityObjectToClipPos(i.vertPos);  
-                o.fragData = float4(i.vertPos.xyzw);
-                return o;  //最终输出这个结构体
-           } 
-           
-           //编写片元着色器函数。是一个输出屏幕上每一个像素点的函数。SV_TARGET为该函数输出位置
-           float4 frag(v2f i):SV_TARGET{ 
-               float4 color;
-               color.rgb = normalize(i.fragData);
-               color.a = 1;
-               return color;
-           }
-           
-           ENDCG
-       }
+    
+    SubShader{
+        Tags { "RenderType" = "Opaque"}
+        LOD 200
+        
+        Pass{
+            CGPROGRAM
+            #pragma target 3.0 
+            #pragma vertex vert       //定义一个顶点着色器 
+            #pragma fragment frag     //定义一个片元着色器
+            
+            struct adp_data
+            {
+                float4 vert:POSITION;
+            };
+
+            struct v2f
+            {
+                float4 vert:SV_POSITION;
+                fixed4 color:TEXCOORD0;
+            };
+
+            v2f vert(adp_data i)
+            {
+                v2f o;
+                o.vert = UnityObjectToClipPos(i.vert);
+                o.color = mul(unity_ObjectToWorld,i.vert);
+                return o;
+            }
+
+            fixed4 frag(v2f i):SV_Target
+            {
+                fixed4 outColor = i.color;
+                outColor = saturate(outColor + (0.5,0.5,0.5));
+                return outColor;
+            }
+            
+            ENDCG
+        }
     }
-     
     FallBack "Diffuse"
 }
